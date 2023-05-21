daff951023ee62212dbccd44d6cd0ce39eff1813
Yujun Cho <devhante@gmail.com>
2023-05-21 23:50:44 +0900

    완성

--- src/data.js
+++ src/data.js
@@ -1,18 +1,69 @@
 import image11 from "./images/11.jpg";
 import image12 from "./images/12.jpg";
+import image13 from "./images/13.jpg";
 import image21 from "./images/21.jpg";
 import image22 from "./images/22.jpg";
+import image23 from "./images/23.jpg";
 import image31 from "./images/31.jpg";
 import image32 from "./images/32.png";
+import image33 from "./images/33.jpg";
 import image41 from "./images/41.png";
 import image42 from "./images/42.jpg";
+import image43 from "./images/43.jpg";
 import image51 from "./images/51.jpg";
+import image53 from "./images/53.jpg";
 import image61 from "./images/61.jpg";
+import image63 from "./images/63.jpg";
 import image71 from "./images/71.jpg";
 import image72 from "./images/72.jpg";
+import image73 from "./images/73.jpg";
 import image81 from "./images/81.jpg";
 import image82 from "./images/82.jpg";
+import image83 from "./images/83.jpg";
 import image91 from "./images/91.png";
+import image92 from "./images/92.jpg";
+import image93 from "./images/93.jpg";
+
+import image13_0 from "./images/13_0.jpg";
+import image13_1 from "./images/13_1.jpg";
+import image13_2 from "./images/13_2.jpg";
+import image13_3 from "./images/13_3.jpg";
+import image23_0 from "./images/23_0.jpg";
+import image23_1 from "./images/23_1.jpg";
+import image23_2 from "./images/23_2.jpg";
+import image33_0 from "./images/33_0.jpg";
+import image33_1 from "./images/33_1.jpg";
+import image33_2 from "./images/33_2.jpg";
+import image33_3 from "./images/33_3.jpg";
+import image33_4 from "./images/33_4.jpg";
+import image33_5 from "./images/33_5.jpg";
+import image33_6 from "./images/33_6.jpg";
+import image33_7 from "./images/33_7.jpg";
+import image43_0 from "./images/43_0.jpg";
+import image43_1 from "./images/43_1.jpg";
+import image53_0 from "./images/53_0.jpg";
+import image63_0 from "./images/63_0.jpg";
+import image63_1 from "./images/63_1.jpg";
+import image63_2 from "./images/63_2.jpg";
+import image63_3 from "./images/63_3.jpg";
+import image73_0 from "./images/73_0.jpg";
+import image73_1 from "./images/73_1.jpg";
+import image73_2 from "./images/73_2.jpg";
+import image73_3 from "./images/73_3.jpg";
+import image73_4 from "./images/73_4.jpg";
+import image83_0 from "./images/83_0.jpg";
+import image83_1 from "./images/83_1.jpg";
+import image83_2 from "./images/83_2.jpg";
+import image83_3 from "./images/83_3.jpg";
+import image83_4 from "./images/83_4.jpg";
+import image93_0 from "./images/93_0.jpg";
+import image93_1 from "./images/93_1.jpg";
+import image93_2 from "./images/93_2.jpg";
+import image93_3 from "./images/93_3.jpg";
+import image93_4 from "./images/93_4.jpg";
+import image93_5 from "./images/93_5.jpg";
+import image93_6 from "./images/93_6.jpg";
+import image93_7 from "./images/93_7.jpg";
 
 function getPropName(week, isPrime) {
   let result = "";
@@ -42,7 +93,7 @@ function getPropName(week, isPrime) {
       result = isPrime ? "알루미늄호일" : "유성마커";
       break;
     case "9":
-      result = isPrime ? "스티로폼" : "?";
+      result = isPrime ? "스티로폼" : "금속";
       break;
   }
 
@@ -77,7 +128,7 @@ function getPropImage(week, isPrime) {
       result = isPrime ? image81 : image82;
       break;
     case "9":
-      result = isPrime ? image91 : image91;
+      result = isPrime ? image91 : image92;
       break;
   }
 
@@ -93,6 +144,7 @@ function getContents(week, isPrime) {
       "하얗다.",
       "베이기 쉽다.",
       "접힌 자국이 남는다.",
+      "찢어지기 쉽다",
       "대부분 사각형 모양이다.",
     ],
     [
@@ -379,13 +431,17 @@ function getContents(week, isPrime) {
     ],
     [
       "물에 뜬다.",
-      "표면이 울퉁불퉁해서 꼼꼼이 칠해지지 않는다.",
       "깔끔하게 자르기 쉽지 않다.",
       "열을 가하면 깔끔하게 자를 수 있다.",
       "시간이 지나면 눌러서 들어간 부분이 다시 돌아온다.",
+      "표면이 울퉁불퉁해서 꼼꼼이 칠해지지 않는다.",
     ],
   ];
-  const content92 = [[], [], []];
+  const content92 = [
+    ["튼튼하다.", "전기가 잘 통한다.", "열을 가하면 금방 뜨거워진다."],
+    ["빛을 비추면 반사해서 반짝거린다.", "얇고 날카롭게 가공할 수 있다."],
+    ["달구어진 금속으로 스티로폼을 깔끔하게 자를 수 있다."],
+  ];
 
   switch (week) {
     case "1":
@@ -420,4 +476,167 @@ function getContents(week, isPrime) {
   return result;
 }
 
-export { getPropName, getPropImage, getContents };
+function getCardText(week) {
+  let result = "";
+  switch (week) {
+    case "1":
+      result = `저희 조에서는 2주차의 주제 물성 '종이'와 어울리는 부제 물성으로 '돌'을 선택했습니다. 종이를 만드는 과정에서 돌이 들어간다는 이유에서였습니다.
+종이와 돌을 가지고 어떻게 작품을 만들까 고민하다가 교수님께서 나눠주신 준비물 중에 유성매직을 돌에 칠해보는 과정에서 '색을 입히기 힘들다'라는 물성을 알게 되었습니다.
+그에 비해 종이는 '색을 입히기 쉽다'라는 물성을 가지고 있었습니다. 돌의 겉면을 종이로 감싸 유성매직으로 칠한다면 돌의 '변형시키기 힘들다'라는 물성으로 인해 적은 양의 종이만으로는 만들 수 없는 입체적이고 형태가 쉽게 바뀌지 않는 작품을 만들 수 있을 것이라 생각했습니다.
+제가 가져온 돌 중에 크기가 적당하고 예쁜 돌을 하나 선택하여 겉면을 종이로 감쌌습니다. 그 모양이 마치 계란같이 보여 어렸을 때 즐겨 봤던 애니메이션인 '캐릭캐릭 체인지'에 등장하는 '수호캐릭터 알'이 연상되어 한쪽 면을 애니메이션에 등장하는 주인공의 수호캐릭터 알로 칠하고, 반대쪽 면을 악당 역할의 수호캐릭터 알로 칠하여 양면성을 표현해 보았습니다.
+추가로 종이의 물성인 '찢어지기 쉽다'를 보완하기 위해 박스테이프의 물성인 '투명하다', '얇다', '종이에 잘 붙는다'를 이용하여 겉을 박스테이프로 감싸 내구성을 높였습니다.`;
+      break;
+    case "2":
+      result = `저희 조에서는 3주차의 주제 물성 '연필'과 어울리는 부제 물성으로 '물풀'을 선택했습니다.
+저는 연필의 물성을 탐구하기 위해 연필깎기로 깎아보기도 하고, 부러뜨려 보기도 했습니다. 그 과정에서 연필이 소모되고 결국 수명이 다해 버려지는 과정을 연상하게 되었습니다.
+대부분의 물건들은 수명이 다 해서 버려지더라도 원래 형태와 비슷한 상태로 버려지는데, 연필은 깎여나간 모습이 그 역할이 다해 버려진 상태라는 생각이 들었고, 이것이 다른 물건들과 다른 매력적인 점이라고 생각하여 그 부분을 작품에 집중적으로 표현하였습니다.
+물풀의 물성인 '투명하다', '물체에 바르고 그 위에 다른 물체를 붙이면 잘 붙는다'를 이용하여 종이 위에 물풀을 발라 중앙에 부러진 연필을 붙이고 주변에는 연필을 깎고 남은 조각들을 붙여 역할이 다해 버려진 연필의 두 가지 상태를 표현해 보았습니다.`;
+      break;
+    case "3":
+      result = `저희 조에서는 4주차의 주제 물성 '크레파스'와 어울리는 부제 물성으로 '양초'를 선택했습니다.
+크레파스와 양초의 녹았을 때의 물성을 탐구하기 위해 잘게 자른 크레파스를 국자에 담고, 불을 붙인 양초 위에 올려두어 녹여 보았습니다.
+크레파스의 '색깔이 다양하다', '녹았을 때 금방 굳는다'라는 물성을 이용하여 실링 왁스를 직접 만들어 보았습니다.
+처음에는 양초와 크레파스를 섞어 녹여 보았는데, 점성이 너무 낮아 동그란 모양이 되지 않고 넓게 퍼져 실링 왁스로 사용할 수 없었습니다.
+점성을 높이기 위해 글루건 심을 일정량 섞으니 적절한 점성을 가지게 되었습니다.
+지우개를 칼로 파서 스탬프를 만들어 굳지 않은 실링 왁스를 눌러 주었고, 굳은 이후에 다시 떼는 것까지 성공하였습니다.
+여러 가지 준비물의 물성을 이용하여 하나의 작품을 완성하는 과정이 즐거웠습니다.
+특히 여러 번의 시행착오 끝에 적절한 점성의 실링 왁스를 만들기 위한 크레파스, 양초, 글루건 심의 비율을 찾아내는 것이 즐거웠습니다.`;
+      break;
+    case "4":
+      result = `저희 조에서는 5주차의 주제 물성 '박스테이프'와 어울리는 부제 물성으로 '색종이'를 선택했습니다.
+조원들이 각자 여러 가지 색종이를 가져왔는데, 저는 학종이를 준비해 왔습니다. 살면서 한번도 종이학을 접어본 적이 없어서 학을 접을 줄 아는 조원분께 부탁하여 직접 학을 접어 보았습니다. 그 과정에서 색종이의 '접어서 여러 가지 형태를 만들 수 있다'라는 물성을 알게 되었습니다.
+종이학과 박스테이프를 이용하여 어떤 작품을 만들까 고민하다가 박스테이프의 물성인 '투명하다', '접착 능력이 있다', '여러 번 접으면 단단해지고 구부러지지 않는다', '가벼운 물체를 세울 수 있다'를 이용하여 색종이를 잘라 만든 태양과 산, 연못을 입체적으로 배치하여 팝업북처럼 표현해 보았습니다.
+접은 종이학을 연못에서 목을 축이는 것처럼 보이도록 하였고, 작품의 이름을 동요의 한 구절을 인용하여 '깊은 산 속 옹달샘 누가 와서 먹나요'로 재치있게 정했습니다.`;
+      break;
+    case "5":
+      result = `저는 6주차의 주제 물성인 '자연물' 3가지를 '나뭇가지', '솔방울', '냉이'로 정했습니다.
+솔방울의 물성을 탐구하기 위해 솔방울의 잎을 떼어보다가, 솔방울의 잎이 마치 비늘같기도 하고 깃털같기도 해서 솔방울의 잎을 이어붙여 날개를 만들어 보기로 결정했습니다.
+솔방울의 잎을 하나하나 떼어 목공풀로 접착했습니다. 목공풀이 마르는 속도가 빠르지 않아서 한 짝밖에 만들지 못하였습니다. 한 짝의 날개를 보고 있자니 그리스 로마 신화의 이카루스가 떠올랐습니다. 직접 만든 인공적인 날개라는 부분에서 신화와 제 작품과의 공통점이 있었고, 이카루스의 날개는 녹아서 떨어졌지만 떨어졌다는 부분에서도 유사한 부분이 있다고 생각했습니다.
+다만 이카루스의 신화는 결말이 단순히 실패로 끝나는데, 저는 실패한 도전에도 의미를 부여하고 싶어 떨어진 날개에서 새로운 생명이 자라나는 것을 냉이 뿌리와 잎으로 표현해 보았습니다.`;
+      break;
+    case "6":
+      result = `저희 조에서는 7주차의 주제 물성 '나무토막'을 부제 물성 없이 탐구하기로 결정했습니다.
+나무토막의 물성을 탐구하다 '서로 두드리면 소리가 난다', '어떻게 두드리냐에 따라 매번 다른 소리가 난다'라는 물성을 알게 되었고 이를 이용해 여러 가지 소리를 내는 타악기를 만들어 작은 연주회를 하기로 하였습니다.
+세 가지 악기를 만들었는데, 첫 번째는 직육면체 모양의 가공된 나무토막을 얇은 나무젓가락으로 두드려서 드럼과 같은 소리를 내는 악기를 만들었습니다.
+두 번째는 물통 안에 부러뜨린 나무젓가락을 여러 개 넣어 마라카스와 비슷한 소리를 내는 악기를 만들었습니다.
+세 번째는 나무 젓가락 여러 개를 이어붙여 만든 두꺼운 나무토막으로 넓은 나무주걱을 두드리거나 긁어 좀 더 낮고 울리는 소리를 내는 타악기를 만들었습니다.
+인간의 목소리와 나무토막으로 만든 타악기가 합쳐지니 잘 어울리고 듣기에 좋아 신나게 연주할 수 있었던 것 같습니다.`;
+      break;
+    case "7":
+      result = `저희 조에서는 8주차의 주제 물성 '솜'과 어울리는 부제 물성으로 '이쑤시개'를 선택했습니다. 솜의 부드러운 물성과 반대되는 뾰족한 물성을 가졌기 때문입니다.
+교수님이 주신 솜과 조원이 가져온 솜을 비교해 보니 겉으로는 차이가 없지만 섬유의 종류가 달라 질감이나 뭉쳐지는 정도 등 물성의 차이를 알 수 있었습니다.
+처음에는 이쑤시개와 뭉친 솜을 여러 개 만들어 수세미에 꽂아 민들레 꽃씨를 만들어 보려 했으나 이쑤시개의 고정력이 약해 실패했습니다.
+솜의 물성인 '촉감이 부드럽다', 이쑤시개의 물성인 '접착제 없이도 솜과 잘 붙는다'를 이용하여 솜을 동그랗게 뭉친 후 이쑤시개로 연결하여 강아지를 만들어 보았습니다. 솜의 물성을 탐구하다가 '펜으로 색칠할 수 있다'라는 물성을 알게 되어 이 물성을 활용하여 강아지의 눈과 코 부분을 검은색 매직으로 칠한 솜을 작게 뭉쳐서 만들었습니다. 아쉬웠던 점은 가벼워서 바람이 조금만 불어도 쓰러진다는 것과, 접착제를 쓰지 않아 쉽게 형태가 무너진다는 것이었습니다.`;
+      break;
+    case "8":
+      result = `저희 조에서는 9주차의 주제 물성 '알루미늄 호일'과 어울리는 부제 물성으로 '유성마커(유성매직)'를 선택했습니다.
+알루미늄 호일의 물성을 탐구하다가 한 조원분이 요즘 유행하는 발렌시아가 패션쇼를 패러디한 유튜브 동영상을 보여 주셨고, 발렌시아가 패션쇼에 쓰이는 의상들의 독특함을 알루미늄 호일로 표현하면 어떨까 하는 아이디어에서 시작하여 조원들이 다같이 알루미늄 호일과 유성마커로 독특한 의상을 만들어 패션쇼를 하기로 하였습니다.
+알루미늄 호일의 물성인 '얇다', '빛에 반사된다', '잘 뭉쳐져서 모양을 잡기가 쉽다'와 유성마카의 물성인 '알루미늄 호일 위에 잘 써진다'를 이용하여 여러 의상을 만들어 보았습니다. 선글라스는 안경 위에 독특한 모양의 호일을 씌운 후 빛이 반사되는 만화적 느낌의 장식을 유성마카로 표현해 주었고, 장갑은 제 손을 테이프로 감싼 후 다시 그 위를 알루미늄 호일로 감싼 다음 조심히 빼서 만들었습니다. 가방과 머리띠에는 발렌시아가의 로고를 마카로 칠해 넣었고, 목걸이는 실제로 있는 제품을 모방한 것으로 노란색 마카로 금색 체인을 표현했습니다.
+수업 막바지에 다같이 만든 의상을 착용하고 패션쇼를 했는데 많은 사람들이 좋아해 주어서 뿌듯한 기분이 들었습니다.`;
+      break;
+    case "9":
+      result = `저희 조에서는 10주차의 주제 물성 '스티로폼'과 어울리는 부제 물성으로 '금속'을 선택했습니다.
+마지막 작품이라 조원들이 다같이 의미를 가진 작품을 만들면 어떨까 하는 의견이 나왔고, 스티로폼이 주로 일회용품으로 쓰인다는 점을 이용하여 이글루를 만들기로 했습니다.
+처음에는 커터칼로 스티로폼을 잘라보려고 하였으나, 깔끔하게 잘리지 않고 조각들이 지저분하게 튀었습니다. 스티로폼의 물성인 '열을 가하면 깔끔하게 자를 수 있다'를 이용하여 열을 가한 금속으로 스티로폼을 깔끔하게 잘라 만든 스티로폼 벽돌을 글루건을 이용하여 붙여 만들었습니다.
+지구온난화 문제를 표현하고자 발표할 때 열을 가한 금속을 이용하여 스티로폼으로 만든 이글루를 녹이려고 하였으나 시간상 하지 못해서 아쉬웠습니다.`;
+      break;
+  }
+  return result;
+}
+
+function getCardImage(week) {
+  let result = "";
+  switch (week) {
+    case "1":
+      result = image13;
+      break;
+    case "2":
+      result = image23;
+      break;
+    case "3":
+      result = image33;
+      break;
+    case "4":
+      result = image43;
+      break;
+    case "5":
+      result = image53;
+      break;
+    case "6":
+      result = image63;
+      break;
+    case "7":
+      result = image73;
+      break;
+    case "8":
+      result = image83;
+      break;
+    case "9":
+      result = image93;
+      break;
+  }
+
+  return result;
+}
+
+function getCardImages(week) {
+  let result = [];
+  switch (week) {
+    case "1":
+      result = [image13_0, image13_1, image13_2, image13_3];
+      break;
+    case "2":
+      result = [image23_0, image23_1, image23_2];
+      break;
+    case "3":
+      result = [
+        image33_0,
+        image33_1,
+        image33_2,
+        image33_3,
+        image33_4,
+        image33_5,
+        image33_6,
+        image33_7,
+      ];
+      break;
+    case "4":
+      result = [image43_0, image43_1];
+      break;
+    case "5":
+      result = [image53_0];
+      break;
+    case "6":
+      result = [image63_0, image63_1, image63_2, image63_3];
+      break;
+    case "7":
+      result = [image73_0, image73_1, image73_2, image73_3, image73_4];
+      break;
+    case "8":
+      result = [image83_0, image83_1, image83_2, image83_3, image83_4];
+      break;
+    case "9":
+      result = [
+        image93_0,
+        image93_1,
+        image93_2,
+        image93_3,
+        image93_4,
+        image93_5,
+        image93_6,
+        image93_7,
+      ];
+      break;
+  }
+
+  return result;
+}
+
+export {
+  getPropName,
+  getPropImage,
+  getContents,
+  getCardText,
+  getCardImage,
+  getCardImages,
+};
--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,17 +1,65 @@
 import "./Content.css";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import title from "../images/title.png";
 import { Link, useParams } from "react-router-dom";
-import { getPropName, getPropImage, getContents } from "../data";
+import {
+  getPropName,
+  getPropImage,
+  getContents,
+  getCardText,
+  getCardImage,
+  getCardImages,
+} from "../data";
+import SimpleImageSlider from "react-simple-image-slider";
 
 function Content() {
   let { code } = useParams();
   const week = code[0];
   const page = code[1];
 
+  const [imageslider, setImageslider] = useState(false);
+
   const contents = getContents(week, page === "1");
-  console.log(contents);
   const propImage = getPropImage(week, page === "1");
+  const cardImage = getCardImage(week);
+  const cardImages = getCardImages(week);
+
+  const handleClickCardimage = () => {
+    setImageslider(true);
+  };
+
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setImageslider(false);
+    console.log("click");
+  });
+
+  function useOnClickOutside(ref, handler) {
+    useEffect(
+      () => {
+        const listener = (event) => {
+          // Do nothing if clicking ref's element or descendent elements
+          if (!ref.current || ref.current.contains(event.target)) {
+            return;
+          }
+          handler(event);
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return () => {
+          document.removeEventListener("mousedown", listener);
+          document.removeEventListener("touchstart", listener);
+        };
+      },
+      // Add ref and handler to effect dependencies
+      // It's worth noting that because passed in handler is a new ...
+      // ... function on every render that will cause this effect ...
+      // ... callback/cleanup to run every render. It's not a big deal ...
+      // ... but to optimize you can wrap handler in useCallback before ...
+      // ... passing it into this hook.
+      [ref, handler]
+    );
+  }
 
   return (
     <div className="Content">
@@ -34,51 +82,80 @@ function Content() {
           </Link>
         </div>
       </div>
-      <div className="content">
-        <div className="propinfo">
-          <div
-            className="propimage"
-            style={{ backgroundImage: `url(${propImage})` }}
-          />
-          <div className="proptext">
-            {week}주차 수업
-            <br />
-            {getPropName(week, page === "1")}
+      {page === "1" || page === "2" ? (
+        <div className="content">
+          <div className="propinfo">
+            <div
+              className="propimage"
+              style={{ backgroundImage: `url(${propImage})` }}
+            />
+            <div className="proptext">
+              {Number(week) + 1}주차 수업
+              <br />
+              {getPropName(week, page === "1")}
+            </div>
+          </div>
+          <div className="cards">
+            <div className="cardwrap">
+              <div className="cardtitle">객관적 정보</div>
+              <div className="card">
+                <ul>
+                  {contents[0].map((value, key) => (
+                    <li key={key}>{value}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="cardwrap">
+              <div className="cardtitle">관찰과 체험으로 얻은 정보</div>
+              <div className="card">
+                <ul>
+                  {contents[1].map((value, key) => (
+                    <li key={key}>{value}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="cardwrap">
+              <div className="cardtitle">예상외의 정보</div>
+              <div className="card">
+                <ul>
+                  {contents[2].map((value, key) => (
+                    <li key={key}>{value}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="cards">
-          <div className="cardwrap">
-            <div className="cardtitle">객관적 정보</div>
-            <div className="card">
-              <ul>
-                {contents[0].map((value, key) => (
-                  <li key={key}>{value}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="cardwrap">
-            <div className="cardtitle">관찰과 체험으로 얻은 정보</div>
-            <div className="card">
-              <ul>
-                {contents[1].map((value, key) => (
-                  <li key={key}>{value}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="cardwrap">
-            <div className="cardtitle">예상외의 정보</div>
-            <div className="card">
-              <ul>
-                {contents[2].map((value, key) => (
-                  <li key={key}>{value}</li>
-                ))}
-              </ul>
-            </div>
+      ) : (
+        <div className="content">
+          <div className="card">
+            <div
+              className="cardimage"
+              style={{ backgroundImage: `url(${cardImage})` }}
+              onClick={handleClickCardimage}
+            ></div>
+            <div className="space" />
+            <div className="cardtext">{getCardText(week)}</div>
           </div>
         </div>
-      </div>
+      )}
+      {imageslider ? (
+        <div className="imageslider">
+          <div className="imageslider-wrap" ref={ref}>
+            <SimpleImageSlider
+              width={600}
+              height={600}
+              images={cardImages}
+              showBullets={true}
+              showNavs={true}
+            />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
